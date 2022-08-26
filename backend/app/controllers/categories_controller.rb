b8dda727191aa2ae9d6e5dc89d37adea0812ba14
Pawel Biegun <bigunpawel900@gmail.com>
2022-08-26 07:03:17 +0200

    implement CRUD operations for categories

--- backend/app/controllers/categories_controller.rb
+++ backend/app/controllers/categories_controller.rb
@@ -1,2 +1,65 @@
 class CategoriesController < ApplicationController
+    def show 
+        begin
+            @category = Category.find(params.fetch(:id, nil))
+        rescue
+            render status: "400", json: {"message": "category not found"}
+            return
+        end 
+
+        render status: "200", json: @category
+    end
+
+    def create
+        begin
+            parsed_schema = JSON.parse(params.fetch(:schema, nil))
+        rescue => exception
+            render status: "400", json: {"message": "unable to parse schema"}.to_json()
+        end
+
+        @category = Category.new(
+            parent_category_id: params.fetch(:parent_category_id, nil),
+            name: params.fetch(:name, nil),
+            schema: params.fetch(:schema, nil)
+        )
+
+        if @category.save
+            render status: "201", json: @category
+        else
+            render status: "400", json: {"errors": @category.errors.full_messages}
+        end
+    end
+
+    def index 
+        @categories = Category.all 
+
+        render status: 200, json: @categories
+    end
+
+    def update
+        begin
+            @category = Category.find(params.fetch(:id, nil))
+        rescue
+            render status: "400", json: {"message": "category not found"}
+            return
+        end
+
+        if @category.update!(params.permit(:schema, :name, :parent_category_id))
+            render status: "200", json: @category
+        else 
+            render status: "400", json: {"errors": @category.errors.full_messages}
+        end
+    end
+
+    def destroy
+        begin
+            @category = Category.find(params.fetch(:id, nil))
+        rescue
+            render status: "400", json: {"message": "category not found"}
+            return
+        end
+
+        @category.destroy
+        render status: "200", json: {"message": "category deleted"}
+    end
 end
