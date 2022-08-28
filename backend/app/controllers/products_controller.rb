b3b692a34a7a5d2322859dfae6e93379f1bbabe9
Pawel Biegun <bigunpawel900@gmail.com>
2022-08-28 13:55:24 +0200

    finish work on data integrity

--- backend/app/controllers/products_controller.rb
+++ backend/app/controllers/products_controller.rb
@@ -16,9 +16,41 @@ class ProductsController < ApplicationController
             return
         end
 
+        category_tree = [@category]
+        category_tree << Category.find(@category.parent_category_id) #each created category has at least one parent category
+        iteration = 1
+        while true
+            if category_tree[iteration].parent_category_id.nil? == true 
+                break
+            end
+            category_tree << Category.find(category_tree[iteration].parent_category_id)
+            iteration += 1
+        end
+
         @product.category = @category
 
         if @product.save
+            category_tree.each do |category|
+                values = {}
+                parsed_schema = JSON.parse(category.schema)
+                parsed_schema.each do |key, value|
+                    if value == "BOOLEAN"
+                        values[key] = false
+                    end
+                    if value == "DECIMAL"
+                        values[key] = 0.0
+                    end
+                    if value == "INTEGER"
+                        values[key] = 0
+                    end
+                end
+
+                CategoryProductInfo.create(
+                    category_id: category.id,
+                    product_id: @product.id,
+                    values: values.to_json
+                )
+            end
             render status: "201", json: @product
         else
             render status: "400", json: {"errors": @product.errors.full_messages}
@@ -37,7 +69,12 @@ class ProductsController < ApplicationController
             render status: "400", json: {"message": "category not found"}
             return
         end
-        
+
+        if @product.category.id != @category.id 
+            render status: "400", json: {"message": "cannot mutate category id, if you want to change this recreate the product"}
+            return
+        end
+
         if @product.update!(params.permit(:name))
             render status: "200", json: @product
         else 
