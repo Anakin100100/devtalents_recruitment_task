8753335e6397733792f39c28a551e8aa4548f384
Paweł Biegun <biegunpawel900@gmail.com>
2022-08-28 21:42:52 +0200

    add filters on the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,10 +24,21 @@ export default function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [currentCategoryId, setCurrentCategoryId] = useState(0);
   const [currentCategoryName, setCurrentCategoryName] = useState("");
+  const [firstFilterField, setFirstFilterField] = useState("");
+  const [firstFilterOperator, setFirstFilterOperator] = useState("");
+  const [firstFilterValue, setFirstFilterValue] = useState("");
+  const [secondFilterField, setSecondFilterField] = useState("");
+  const [secondFilterOperator, setSecondFilterOperator] = useState("");
+  const [secondFilterValue, setSecondFilterValue] = useState("");
+  const [thirdFilterField, setThirdFilterField] = useState("");
+  const [thirdFilterOperator, setThirdFilterOperator] = useState("");
+  const [thirdFilterValue, setThirdFilterValue] = useState("");
+  const [filtersError, setFiltersError] = useState("");
   const [history, setHistory] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
+      console.log("using use effect");
       setIsLoading(true);
       let current_id = currentCategoryId;
       if (currentCategoryId === 0) {
@@ -122,6 +133,141 @@ export default function App() {
                   </button>
                 </div>
               ))}
+              <div className="flex">
+                <p>Filters:</p>
+              </div>
+              <div>
+                <table>
+                  <tbody>
+                    <tr>
+                      <th>Field</th>
+                      <th>Operator</th>
+                      <th>Value</th>
+                    </tr>
+                    <tr>
+                      <th>
+                        <input
+                          value={firstFilterField}
+                          onChange={(e) => setFirstFilterField(e.target.value)}
+                          className="shadow appearance-none border border-red-500 rounded w-full py-1 px-2 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                      </th>
+                      <th>
+                        <input
+                          value={firstFilterOperator}
+                          onChange={(e) =>
+                            setFirstFilterOperator(e.target.value)
+                          }
+                          className="shadow appearance-none border border-red-500 rounded w-full py-1 px-2 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                      </th>
+                      <th>
+                        <input
+                          value={firstFilterValue}
+                          onChange={(e) => setFirstFilterValue(e.target.value)}
+                          className="shadow appearance-none border border-red-500 rounded w-full py-1 px-2 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                      </th>
+                    </tr>
+                    <tr>
+                      <th>
+                        <input
+                          value={secondFilterField}
+                          onChange={(e) => setSecondFilterField(e.target.value)}
+                          className="shadow appearance-none border border-red-500 rounded w-full py-1 px-2 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                      </th>
+                      <th>
+                        <input
+                          value={secondFilterOperator}
+                          onChange={(e) =>
+                            setSecondFilterOperator(e.target.value)
+                          }
+                          className="shadow appearance-none border border-red-500 rounded w-full py-1 px-2 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                      </th>
+                      <th>
+                        <input
+                          value={secondFilterValue}
+                          onChange={(e) => setSecondFilterValue(e.target.value)}
+                          className="shadow appearance-none border border-red-500 rounded w-full py-1 px-2 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                      </th>
+                    </tr>
+                    <tr>
+                      <th>
+                        <input
+                          value={thirdFilterField}
+                          onChange={(e) => setThirdFilterField(e.target.value)}
+                          className="shadow appearance-none border border-red-500 rounded w-full py-1 px-2 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                      </th>
+                      <th>
+                        <input
+                          value={thirdFilterOperator}
+                          onChange={(e) =>
+                            setThirdFilterOperator(e.target.value)
+                          }
+                          className="shadow appearance-none border border-red-500 rounded w-full py-1 px-2 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                      </th>
+                      <th>
+                        <input
+                          value={thirdFilterValue}
+                          onChange={(e) => setThirdFilterValue(e.target.value)}
+                          className="shadow appearance-none border border-red-500 rounded w-full py-1 px-2 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                      </th>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className="flex">
+                <button
+                  onClick={async () => {
+                    const products_options = {
+                      method: "GET",
+                      body: {
+                        filters: [
+                          {
+                            field: firstFilterField,
+                            operator: firstFilterOperator,
+                            value: firstFilterValue,
+                          },
+                          {
+                            field: secondFilterField,
+                            operator: secondFilterOperator,
+                            value: secondFilterValue,
+                          },
+                          {
+                            field: thirdFilterField,
+                            operator: thirdFilterOperator,
+                            value: thirdFilterValue,
+                          },
+                        ],
+                      },
+                    };
+                    let products_response = await fetch(
+                      `http://localhost:3000/get_products_from_a_category_and_all_subcategories/${currentCategoryId}`,
+                      products_options
+                    );
+                    let products_data = await products_response.json();
+                    if (products_response.ok === true) {
+                      console.log("products:");
+                      console.log(products_data);
+                      setProducts(products_data);
+                    } else {
+                      setFiltersError(products_data);
+                    }
+                  }}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+                >
+                  Apply Filters
+                </button>
+              </div>
+              <div className="flex">
+                <p>{filtersError}</p>
+              </div>
             </div>
           )}
         </div>
