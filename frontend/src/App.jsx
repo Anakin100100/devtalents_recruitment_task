c8dcf85bf036b15d497106ea78c49c6481669272
Paweł Biegun <biegunpawel900@gmail.com>
2022-08-28 19:51:42 +0200

    save

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,67 +1,77 @@
 import ButtonList from "./components/ButtonList";
 import Header from "./components/Header";
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import LoadingSpinner from "./components/LoadingSpinner";
 
 export default function App() {
   const links_data = [
     {
-      "link_to": "/categories",
-      "link_text": "Categories"
+      link_to: "/categories",
+      link_text: "Categories",
     },
     {
-      "link_to": "/products",
-      "link_text": "Products"
+      link_to: "/products",
+      link_text: "Products",
     },
     {
-      "link_to": "/category_product_infos",
-      "link_text": "Product Category Infos"
-    }
-  ]
+      link_to: "/category_product_infos",
+      link_text: "Product Category Infos",
+    },
+  ];
 
-  const [categories, setCategories] = useState({})
-  const [products, setProducts] = useState({})
+  const [categories, setCategories] = useState({});
+  const [products, setProducts] = useState({});
   const [isLoading, setIsLoading] = useState(true);
-  const [currentCategoryId, setCurrentCategoryId] = useState(0)
-  const [currentCategoryName, setCurrentCategoryName] = useState("")
-  const [history, setHistory] = useState([])
+  const [currentCategoryId, setCurrentCategoryId] = useState(0);
+  const [currentCategoryName, setCurrentCategoryName] = useState("");
+  const [history, setHistory] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      setIsLoading(true)
-      let current_id = currentCategoryId
-
+      setIsLoading(true);
+      let current_id = currentCategoryId;
       if (currentCategoryId === 0) {
-        console.log("started data fetch")
+        console.log("started data fetch");
 
-        const current_category_request_options = { method: 'GET' };
+        const current_category_request_options = { method: "GET" };
 
-        let current_category_request_response = await fetch(`http://localhost:3000/get_root_category_id`, current_category_request_options)
-        let current_category_request_data = await current_category_request_response.json()
-        setCurrentCategoryId(current_category_request_data.id)
-        setCurrentCategoryName(current_category_request_data.name)
-        current_id = current_category_request_data.id.id
-        console.log(`current category id is ${current_id}`)
+        let current_category_request_response = await fetch(
+          `http://localhost:3000/get_root_category_id`,
+          current_category_request_options
+        );
+        let current_category_request_data =
+          await current_category_request_response.json();
+        console.log("response data");
+        console.log(current_category_request_data.id.id);
+        setCurrentCategoryId(current_category_request_data.id.id);
+        current_id = current_category_request_data.id.id;
+        setCurrentCategoryName(current_category_request_data.id.name);
       }
 
-      setHistory([...history, currentCategoryId])
+      setHistory([...history, currentCategoryId]);
 
-      const options = { method: 'GET' };
-      let response = await fetch(`http://localhost:3000/categories/get_sub_categories/${current_id}`, options)
-      let data = await response.json()
-      console.log("categories:")
-      console.log(data)
-      setCategories(data)
+      const options = { method: "GET" };
+      let response = await fetch(
+        `http://localhost:3000/categories/get_sub_categories/${current_id}`,
+        options
+      );
+      let data = await response.json();
+      console.log("categories:");
+      console.log(data);
+      setCategories(data);
 
-      const products_options = { method: 'GET' };
+      const products_options = { method: "GET" };
 
-      let products_response = await fetch(`http://localhost:3000/get_products_from_a_category_and_all_subcategories/${current_id}`, products_options)
-      let products_data = await products_response.json()
-      console.log("products:")
-      console.log(products_data)
-      setProducts(products_data)
+      let products_response = await fetch(
+        `http://localhost:3000/get_products_from_a_category_and_all_subcategories/${current_id}`,
+        products_options
+      );
+      let products_data = await products_response.json();
+      console.log("products:");
+      console.log(products_data);
+      setProducts(products_data);
 
-      setIsLoading(false)
+      setIsLoading(false);
     };
 
     fetchData();
@@ -72,28 +82,42 @@ export default function App() {
       <Header />
       <ButtonList links={links_data} />
       <div className="flex flex-grow mt-4">
-        <div className="flex bg-cyan-200 w-1/4 justify-center">
+        <div className="flex bg-cyan-200 w-1/4">
           {isLoading ? (
             <LoadingSpinner />
           ) : (
-            <div className="flex-col flex">
+            <div className="flex items-center flex-col flex-grow">
               <div className="flex">
-                <button onClick={() => {
-                  console.log("history")
-                  console.log(history)
-                  let previous_category_id = history[history.length - 2]
-                  console.log(`previous category id: ${previous_category_id}`)
-                  setHistory(history.slice(0, -1))
-                  setCurrentCategoryId(previous_category_id)
-                }}>
+                <button
+                  onClick={() => {
+                    console.log("history");
+                    console.log(history);
+                    let previous_category_id = history[history.length - 2];
+                    console.log(
+                      `previous category id: ${previous_category_id}`
+                    );
+                    setHistory(history.slice(0, -1));
+                    setCurrentCategoryId(previous_category_id);
+                  }}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+                >
                   Go Back
                 </button>
-                <br />
+              </div>
+              <div className="flex">
                 <p>Current category: {currentCategoryName}</p>
+              </div>
+              <div className="flex">
+                <p>Sub Categories:</p>
               </div>
               {categories.map((category) => (
                 <div className="flex" key={category.id}>
-                  <button onClick={() => { setCurrentCategoryId(category.id) }}>
+                  <button
+                    onClick={() => {
+                      setCurrentCategoryId(category.id);
+                    }}
+                    className="font-bold"
+                  >
                     {category.name}
                   </button>
                 </div>
@@ -126,6 +150,6 @@ export default function App() {
           )}
         </div>
       </div>
-    </div >
+    </div>
   );
 }
